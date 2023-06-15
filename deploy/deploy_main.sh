21ea791372c5c444cfc57644764cd00edca8c8b4
sorhawell <sorhawell@gmail.com>
2023-06-15 23:37:09 +0200

    fiddle with deploy

--- deploy/deploy_main.sh
+++ deploy/deploy_main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pwd
-docker build -t ast_shiny_0.1.1 .
+docker build -t ast_shiny_0.1.1 . #update image
+docker image prune -f # remove previous build image
 docker run -v .:/srv/shiny-server/ --rm -p 3838:3838 -p 3839:3839 ast_shiny_0.1.1
